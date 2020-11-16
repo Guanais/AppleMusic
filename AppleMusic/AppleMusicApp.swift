9899ff1dc8f1a55116ebef1cfe253af8e3436ea7
Kaio <kaioaguanais@gmail.com>
2020-11-16 14:30:50 -0800

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Kaio Guanais on 2020-11-16.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
