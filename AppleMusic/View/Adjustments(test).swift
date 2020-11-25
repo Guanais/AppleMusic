73e5dd8bf3c1881598ec83a8c65a8492430470cb
Kaio <kaioaguanais@gmail.com>
2020-11-24 16:27:08 -0800

    Test

--- AppleMusic/View/Adjustments(test).swift
+++ AppleMusic/View/Adjustments(test).swift
@@ -7,27 +7,27 @@
 
 import SwiftUI
 
-struct Adjustments: View {
-    var body: some View {
-            Text("Test")
-                .fontWeight(.bold)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .foregroundColor(.primary)
-                .background(ClearView())
-    }
-}
-
-struct ClearView: UIViewRepresentable {
-    
-    func makeUIView(context: Context) -> UIView {
-        let view = UIView()
-        DispatchQueue.main.async {
-            view.superview?.superview?.backgroundColor = .clear
-        }
-        return view
-    }
-    
-    func updateUIView(_ uiView: UIView, context: Context) {
-        
-    }
-}
+//struct Adjustments: View {
+//    var body: some View {
+//            Text("Test")
+//                .fontWeight(.bold)
+//                .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                .foregroundColor(.primary)
+//                .background(ClearView())
+//    }
+//}
+//
+//struct ClearView: UIViewRepresentable {
+//    
+//    func makeUIView(context: Context) -> UIView {
+//        let view = UIView()
+//        DispatchQueue.main.async {
+//            view.superview?.superview?.backgroundColor = .clear
+//        }
+//        return view
+//    }
+//    
+//    func updateUIView(_ uiView: UIView, context: Context) {
+//        
+//    }
+//}
